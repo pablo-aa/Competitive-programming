408b2851535e29e6047b4b7649b951ba9e3df643
Pablo Araujo <pablo_aa@pablo-aa.local>
2022-04-03 19:59:17 -0300

    Atualizing fast exponentiation code

--- codes/Math/fast-exponentiation.cpp
+++ codes/Math/fast-exponentiation.cpp
@@ -1,12 +1,25 @@
 // Fast Exponentiation
 // O(log n)
-ll fexp(ll b, ll e){
-	if(e == 0){
-		return 1;
-	}
-	ll resp = fexp(b, e/2)%MOD;
-	resp = (resp*resp)%MOD;
-	if(e%2) resp = (b*resp)%MOD;
 
-	return resp;
+ll fexp(ll b, ll e, ll mod) {
+    ll res = 1;
+    b %= mod;
+    while(e){ 
+        if(e & 1LL)
+            res = (res * b) % mod;
+        e = e >> 1LL;
+        b = (b * b) % mod;
+    }
+    return res;
 }
+
+// ll fexp(ll b, ll e){
+// 	if(e == 0){
+// 		return 1;
+// 	}
+// 	ll resp = fexp(b, e/2)%MOD;
+// 	resp = (resp*resp)%MOD;
+// 	if(e%2) resp = (b*resp)%MOD;
+
+// 	return resp;
+// }
