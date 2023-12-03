5334dd6001f35abb3547796b4b9b84eb465049dd
pablo-aa <pabloarruda.araujo@gmail.com>
2023-12-03 17:07:13 -0300

    Added personal template

---
+++ codes/Templates/template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define vi vector<int>
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define sws ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define forn(i, n) for(int i = 0; i < (int)n; i++)
+#define forne(i, a, b) for(int i = a; i <= b; i++)
+#define all(x) x.begin(), x.end()
+#define teto(a, b) ((a+b-1)/(b))
+#define dbg(msg, var) cerr << msg << " " << var << endl;
+ 
+using namespace std;
+ 
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1000000007;
+const ld EPS = 1e-8;
+const ld PI = acos(-1);
+ 
+// End Template //
+ 
+int main(){
+    sws;
+ 
+    return 0;
+}
+ 
